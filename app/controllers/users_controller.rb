2431f950ba953fc26670d55608e5745c64032c45
ri0629 <tr08792@gmail.com>
2024-01-18 22:20:59 +0900

    更新成功後ページ遷移

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,12 @@ class UsersController < ApplicationController
 
   def edit
   end
-  
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+
 end
