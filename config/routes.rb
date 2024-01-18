254bae99703116a8df934c43756ebec7a5c4cf14
ri0629 <tr08792@gmail.com>
2024-01-18 22:02:31 +0900

    編集内容の更新のルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :users, only: [:edit, :update]
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "messages#index"
